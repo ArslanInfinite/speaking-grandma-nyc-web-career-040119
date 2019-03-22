2d092497da32fef0edbaaa9b25554b48aa2ce004
Arslan Ali <ali.arslan319@gmail.com>
2019-03-22 02:31:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,7 @@ def speak_to_grandma(greeting)
   if greeting == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   elsif greeting == greeting.upcase
-
-
+    'NO, NOT SINCE 1938!'
   else
     'HUH?! SPEAK UP, SONNY!'
   end
